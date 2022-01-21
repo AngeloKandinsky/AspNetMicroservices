5675d7ffb91000702f20ea0b3c58d7a269cbfd03
Angel Ibarra <Aibarra@BWMAC-IT001.local>
2022-01-21 14:29:16 -0600

    adding basic structure  of Catalog api

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+               return await _context
+               .Products
+               .FindSync<Product>(p => true)
+               .ToListAsync();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
